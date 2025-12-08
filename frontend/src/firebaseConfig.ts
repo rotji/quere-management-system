fc3c43092c12f1b5ec98cfe6d4a39042c825b6b9
rotji <starrotji@gmail.com>
2025-12-08 10:28:42 +0100

    chore: update firebase config, rules, and fix functions lint errors

---
+++ frontend/src/firebaseConfig.ts
@@ -0,0 +1,12 @@
+// frontend/src/firebaseConfig.ts
+const firebaseConfig = {
+  apiKey: "AIzaSyC5lCT8qZJ_DA1LY-M8rGcd4h7WS8Uq",
+  authDomain: "quere-management-system.firebaseapp.com",
+  projectId: "quere-management-system",
+  storageBucket: "quere-management-system.appspot.com",
+  messagingSenderId: "531866958878",
+  appId: "1:531866958878:web:ba29e1e55c7d7cd1a5c20f",
+  measurementId: "G-H5BFHM4Y4C"
+};
+
+export default firebaseConfig;
